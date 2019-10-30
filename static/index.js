a962de42f5f87fde018a2997f74638731e4d26fc
hyeonggeun2 <dhgudrms12@gmail.com>
2019-10-30 16:41:29 +0900

    edit js

--- static/index.js
+++ static/index.js
@@ -1,3 +1,10 @@
-const img = document.querySelectorAll('img');
-img[0].onclick(alert('펭하'));
-img[1].onclick(alert('치킨소리'));
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+
+img1.addEventListener('click', function(){
+  alert('펭@@@@하@@@@@@');
+});
+
+img2.addEventListener('click', function(){
+  alert('치킨소리');
+});
